84007b73e1b0ae9cb746a33e637d1480635435a0
Mathes K <mathes.btech@gmail.com>
2026-03-23 13:56:33 -0700

    Initial Commit

---
+++ SFNewsApp/SFNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFNewsAppApp.swift
+//  SFNewsApp
+//
+//  Created by Matheswaran Kanagarajan on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct SFNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
